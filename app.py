cf9190da67c0737d5d3ea64b781a3e9a0c102af7
Lucas Oliveira Teles <135617476+ltees@users.noreply.github.com>
2025-04-13 22:31:34 -0300

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-print("SABOR EXPRESS\n")
+import os
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair\n")
+def exibir_nome_programa():
+    print("SABOR EXPRESS\n")
 
-ocpcao_escolhida = input("Escolha uma opção:")
-print(f"Você escolheu a opção {ocpcao_escolhida}")
+def exibir_opcoes():
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurante")
+    print("3. Ativar restaurante")
+    print("4. Sair\n")
+
+def finalizar_app():
+    os.system("cls")
+    print("Finalizando o programa")
+
+def escolher_opcao():
+    ocpcao_escolhida = int(input("Escolha uma opção:"))
+    print(f"Você escolheu a opção {ocpcao_escolhida}")
+
+    if ocpcao_escolhida == 1:
+        print("cadastrar restaurante")
+    elif ocpcao_escolhida == 2:
+        print("Listar restaurante")
+    elif ocpcao_escolhida == 3:
+        print("Ativar restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
+  
