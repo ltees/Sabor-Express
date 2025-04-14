884e385781323969bef7d6971d2b231fe8982ae1
Lucas Oliveira Teles <135617476+ltees@users.noreply.github.com>
2025-04-13 21:52:10 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+print("SABOR EXPRESS\n")
+
+print("1. Cadastrar restaurante")
+print("2. Listar restaurante")
+print("3. Ativar restaurante")
+print("4. Sair\n")
+
+ocpcao_escolhida = input("Escolha uma opção:")
+print(f"Você escolheu a opção {ocpcao_escolhida}")
